bb38b0e2a1d7f5a846b132705268bac0dfd702cf
Carlos Carbonell Etayo <carcaret@icloud.com>
2018-08-19 20:16:52 +0200

    Add injected service

--- src/main/java/com/carcaret/HelloController.java
+++ src/main/java/com/carcaret/HelloController.java
@@ -1,6 +1,7 @@
 package com.carcaret;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,11 +10,14 @@ import javax.ws.rs.core.MediaType;
 @ApplicationScoped
 @Produces(MediaType.TEXT_PLAIN)
 @Path("/")
-public class HelloWorldController {
+public class HelloController {
+
+    @Inject
+    private HelloService service;
 
     @GET
     @Path("/hello")
     public String sayHello() {
-        return "Hello World!";
+        return service.message();
     }
 }
